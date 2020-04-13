86b95de1d1f78b26eb65a1466be5d9f057a13598
zjy26 <2536615714@qq.com>
2020-04-13 22:17:50 +0800

    改写巡检设置form

--- src/pages/patrolConfig/configModal.js
+++ src/pages/patrolConfig/configModal.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Modal, Input, Select } from 'antd';
+import { Form, Modal, Input, Select } from 'antd';
 import { robotConfig } from '../../api'
 
 const formItemLayout = {
@@ -16,58 +14,44 @@ const formItemLayout = {
 }
 
 const ConfigModal = (props) => {
-  const { getFieldDecorator, resetFields } = props.form
-  const [obj, setObj] = useState({})
+  const [form] = Form.useForm()
 
   useEffect(() => {
     //查看详情
-    if(props.visible===true && props.currentId !== 0) {
+    if(props.currentId !== 0) {
       robotConfig.robotConfigDetail(props.currentId)
       .then((res) =>{
         if(res){
-          setObj({...res, siteLine: res.site.slice(0, 4)})
+          form.setFieldsValue({...res, siteLine: res.site.slice(0, 4)})
         }
       })
     } else {
-      setObj({})
-      resetFields()
+      form.resetFields()
     }
-  }, [props.visible, props.currentId, resetFields]);
+  }, [props.visible, props.currentId])
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    const {
-      form: { validateFields, isFieldsTouched },
-    } = props;
-
-    validateFields((errors, values) => {
-      if(errors) {
-        return
-      }
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields()
       let {siteLine, ...data} = values
       let params = {...data}
       if(values.id) {//编辑
-        if(isFieldsTouched() === true) {  //判断是否有修改
-          Modal.confirm({
-            title: '确认提示',
-            content: '是否确认修改？',
-            okText: '确认',
-            okType: 'danger',
-            cancelText: '取消',
-            onOk: ()=> {
-              robotConfig.robotConfigEdit(values.id, params)
-              .then((res)=>{
-                props.handleCancel()
-                props.setDirty((dirty)=>dirty+1)
-              })
-            },
-            onCancel() {
-            },
-          });
-        } else {
-          props.handleCancel()
-          resetFields()
-        }
+        Modal.confirm({
+          title: '确认提示',
+          content: '是否确认修改？',
+          okText: '确认',
+          okType: 'danger',
+          cancelText: '取消',
+          onOk: ()=> {
+            robotConfig.robotConfigEdit(values.id, params)
+            .then((res)=>{
+              props.handleCancel()
+              props.setDirty((dirty)=>dirty+1)
+            })
+          },
+          onCancel() {
+          },
+        })
       } else {//添加
         robotConfig.robotConfigAdd(params)
         .then((res)=>{
@@ -75,12 +59,14 @@ const ConfigModal = (props) => {
           props.setDirty((dirty)=>dirty+1)
         });
       }
-
-    });
+    } catch (errorInfo) {
+      return;
+    }
   }
 
   return (
     <Modal
+      getContainer={false}
       title={props.title}
       okText="确认"
       cancelText="取消"
@@ -90,59 +76,64 @@ const ConfigModal = (props) => {
       currentId={props.currentId}
       locationTree={props.locationTree}
     >
-      <Form {...formItemLayout}>
-        <Form.Item label="id" style={{display: 'none'}}>
-          {getFieldDecorator('id', {
-            initialValue: obj.id,
-          })(<Input />)}
+      <Form form={form} {...formItemLayout}>
+        <Form.Item
+          label="id" 
+          name="id"
+          style={{display: 'none'}}
+        >
+          <Input />
         </Form.Item>
-        <Form.Item label="线路">
-          {getFieldDecorator('siteLine', {
-            initialValue: obj.siteLine,
-            rules: [{required: true}],
-          })(
+        <Form.Item
+          label="线路"
+          name="siteLine"
+          rules= {[{required: true}]}
+        >
           <Select placeholder="请选择线路">
             {props.locationTree.line && props.locationTree.line.map(item => (
               <Select.Option key={item.value} value={item.value}>
                 {item.label}
               </Select.Option>
             ))}
-          </Select>)}
+          </Select>
         </Form.Item>
-        <Form.Item label="站点">
-          {getFieldDecorator('site', {
-            initialValue: obj.site,
-            rules: [{required: true}],
-          })(
+        <Form.Item
+          label="站点"
+          name="site"
+          rules= {[{required: true}]}
+        >
           <Select placeholder="请选择站点">
             {props.locationTree.site && props.locationTree.site.map(item => (
               <Select.Option key={item.value} value={item.value}>
                 {item.label}
               </Select.Option>
             ))}
-          </Select>)}
+          </Select>
         </Form.Item>
-        <Form.Item label="服务器IP">
-          {getFieldDecorator('ip', {
-            initialValue: obj.ip,
-            rules: [{required: true}],
-          })(<Input placeholder="请输入服务器IP"/>)}
+        <Form.Item
+          label="服务器IP"
+          name="ip"
+          rules= {[{required: true}]}
+        >
+          <Input placeholder="请输入服务器IP"/>
         </Form.Item>
-        <Form.Item label="服务器端口">
-          {getFieldDecorator('port', {
-            initialValue: obj.port,
-            rules: [{required: true}],
-          })(<Input placeholder="请输入服务器端口"/>)}
+        <Form.Item
+          label="服务器端口"
+          name="port"
+          rules= {[{required: true}]}
+        >
+          <Input placeholder="请输入服务器端口"/>
         </Form.Item>
-        <Form.Item label="视频流推送">
-          {getFieldDecorator('cameraStreamUrl', {
-            initialValue: obj.cameraStreamUrl,
-            rules: [{required: true}],
-          })(<Input placeholder="请输入视频流推送"/>)}
+        <Form.Item
+          label="视频流推送"
+          name="cameraStreamUrl"
+          rules= {[{required: true}]}
+        >
+          <Input placeholder="请输入视频流推送"/>
         </Form.Item>
       </Form>
     </Modal>
   )
 }
 
-export default Form.create()(ConfigModal);
+export default React.memo(ConfigModal)
