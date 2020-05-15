ca3e0e5102b1da932a3b65b639c0f26269b571d3
hzx <657070781@qq.com>
2020-05-16 00:08:36 +0800

    萤石视频

--- src/pages/home/view.js
+++ src/pages/home/view.js
@@ -1,78 +1,128 @@
 import React, { useState, useEffect } from 'react'
-import {connect} from 'react-redux'
-import { Row, Col, Spin } from 'antd'
-import Axios from 'axios'
-import Error from '../error'
-
+import EZUIKit from "./ezuikit"
+let decoder = null
 const View = (props) => {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [statusCode, setStatusCode] = useState(200)
-  const showModal = ()=>{
-    props.showModal()
-  }
-
-  useEffect(() => {
-    Axios.get('/applications').then(res =>{
-      if(res.status === 200){
-        setData(res.data)
-        setLoading(false)
-      }
-    }).catch((err) =>{
-      setStatusCode(err.response.status)
-    })
-  }, [])
-
-  if(loading === true) {
-    if(statusCode === 500 || statusCode === 404){
-      return(
-        <Error status={statusCode}/>
-      )
-    } else {
-      return (
-      //加载中状态
-      <Spin />
-      )
-    }
-  } else {
-    return (
-      <div>
-        <Row style={{textAlign: 'center'}}>
-          {
-            data.map((item, index)=>{
-              if(item.name === "Password") {
-                return (
-                  <Col xs={12} sm={8} md={6} lg={4} key={index}>
-                    <span>
-                      <img src={item.icon} alt={item.name} onClick={showModal}  />
-                    </span>
-                  </Col>
-                )
-              } else {
-                return (
-                  <Col xs={12} sm={8} md={6} lg={4} key={index}>
-                    <a href={item.href}>
-                      <img src={item.icon} alt={item.name}/>
-                    </a>
-                  </Col>
-                )
-              }
-            })
-          }
-        </Row>
-      </div>
-    );
-  }
-}
-const mapDispatchToProps = (dispatch) => {
-  return {
-      showModal() {
-        let action = {
-          type:'showPsdModal'
+    const handleInit = () => {
+        function handleError(e){
+            console.log('捕获到错误',e)
+            // log(JSON.stringify(e),'error');
+            //alert(e)
         }
-        dispatch(action)
-      }
-  }
-}
+        function handleSuccess(){
+            console.log("播放成功回调函数，此处可执行播放成功后续动作");
+        }
+        var url = "ezopen://open.ys7.com/203751922/1.live";
+        var accessToken = "at.8o2k6dbpcvtr13reaa96hbnya6fee2wf-9gu6zcjmh2-1j4yrsb-imvlc5poc";
+        decoder = new EZUIKit.EZUIPlayer({
+            id: 'playWind',
+            autoplay: true,
+            url: url,
+            accessToken: accessToken,
+            decoderPath: '',
+            width: 600,
+            height: 400,
+            handleError: handleError,
+            handleSuccess: handleSuccess,
+        });
+    }
+    const handleStart = () => {
+        function handleError(e){
+            console.log('handleError',e)
+        }
+        function handleSuccess(){
+            console.log('handleSuccess')
+        }
+        decoder.play({
+            handleError: handleError,
+        });
+    }
+    const handleEnd = () => {
+        /*停止播放方法1*/
+        // decoder.stop();
+        /*停止播放方法2 - promise模式*/
+        var stopPromise = decoder.stop();
+        stopPromise.then(function(){
+            console.log("关闭成功，用户执行关闭成功后的操作");
+        })
+    }
+    const getOSDTime =() => {
+        var getOSDTimePromise = decoder.getOSDTime();
+        console.log(getOSDTimePromise)
+        getOSDTimePromise.then(function(data){
+            console.log("getOSDTime success",data)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const handleOpenSound =  () => {
+        decoder.openSound();
+    }
+    const handleCloseSound =  () => {
+        decoder.openSound();
+    }
+    const capturePicture = () => {
+        var capturePicturePromise = decoder.capturePicture(0,'default');
+        capturePicturePromise.then(function(data){
+            console.log("截图成功，用户执行关闭成功后的操作",data);
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const startSave = () => {
+        var startSavePromise = decoder.startSave(0, (new Date().getTime() + 'video'));
+        startSavePromise.then(function(data){
+            console.log("start save success",startSavePromise)
+        })
+            .catch(function(error){
+                console.log("start Save error",error)
+            })
+    }
+    const stopSave = () => {
+        /*结束录制方法1*/
+        // decoder.stopSave(0);
+        /*结束录制方法2*/
+        var stopSavePromise = decoder.stopSave(0);
+        stopSavePromise.then(function(data){
+            console.log("stop save success",stopSavePromise)
+        })
+            .catch(function(error){
+                console.log("stop Save error",error)
+            })
+    }
+    const enableZoom =() => {
+        decoder.enableZoom();
+    }
+    const closeZoom =() => {
+        decoder.closeZoom();
+    }
+    return (
+        <div>
+            {/*<div dangerouslySetInnerHTML={{__html:data}}></div>*/}
+          {/*  <iframe
+                title="resg"
+                srcDoc={data}
+                style={{ width: '100%', border: '0px', height: '1100px' }}
+                sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+                scrolling="auto"
+            />*/}
+            <div className="btn-container">
+                <button id="init" onClick={handleInit}>初始化播放</button>
+                <button id="stop" onClick={handleEnd}>结束</button>
+                <button id="getOSDTime" onClick={getOSDTime}>获取OSD时间</button>
+                <button id="openSound" onClick={handleOpenSound}>打开声音（默认已经开启）</button>
+                <button id="closeSound" onClick={handleCloseSound}>关闭声音</button>
+                <button id="capturePicture" onClick={capturePicture}>视频截图</button>
+                <button id="startSave" onClick={startSave}>开始录像</button>
+                <button id="stopSave" onClick={stopSave}>停止录像</button>
+                <button id="enableZoom" onClick={enableZoom}>开启电子放大</button>
+                <button id="closeZoom" onClick={closeZoom}>关闭电子放大</button>
+                <span>录制功能不支持加密视频，且录制的文件需要<a href="https://service.ys7.com/downloadInfoSite/admin"
+                                             target="_blank">下载海康播放器播放</a></span>
 
-export default connect(null, mapDispatchToProps)(React.memo(View))
+            </div>
+            <div id="playWind">
+            </div>
+        </div>
+    );
+}
+export default React.memo(View)
---
+++ src/pages/patrolConfig/configModal2.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from 'react'
+import { Modal,Form, Input, Select } from 'antd'
+import { robotConfig } from '../../api'
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 6 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+}
+
+const ConfigModal = (props) => {
+  const { getFieldDecorator, resetFields } = props.form
+  const [obj, setObj] = useState({})
+
+  useEffect(() => {
+    //查看详情
+    if(props.visible===true && props.currentId !== 0) {
+      robotConfig.robotConfigDetail(props.currentId)
+      .then((res) =>{
+        if(res){
+          setObj({...res, siteLine: res.site.slice(0, 4)})
+        }
+      })
+    } else {
+      setObj({})
+      resetFields()
+    }
+  }, [props.visible, props.currentId, resetFields]);
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const {
+      form: { validateFields, isFieldsTouched },
+    } = props;
+
+    validateFields((errors, values) => {
+      if(errors) {
+        return
+      }
+      let {siteLine, ...data} = values
+      let params = {...data}
+      if(values.id) {//编辑
+        if(isFieldsTouched() === true) {  //判断是否有修改
+          Modal.confirm({
+            title: '确认提示',
+            content: '是否确认修改？',
+            okText: '确认',
+            okType: 'danger',
+            cancelText: '取消',
+            onOk: ()=> {
+              robotConfig.robotConfigEdit(values.id, params)
+              .then((res)=>{
+                props.handleCancel()
+                props.setDirty((dirty)=>dirty+1)
+              })
+            },
+            onCancel() {
+            },
+          });
+        } else {
+          props.handleCancel()
+          resetFields()
+        }
+      } else {//添加
+        robotConfig.robotConfigAdd(params)
+        .then((res)=>{
+          props.handleCancel()
+          props.setDirty((dirty)=>dirty+1)
+        });
+      }
+
+    });
+  }
+
+  return (
+    <Modal
+      title={props.title}
+      okText="确认"
+      cancelText="取消"
+      visible={props.visible}
+      onCancel={props.handleCancel}
+      onOk={handleSubmit}
+      currentId={props.currentId}
+      locationTree={props.locationTree}
+    >
+      <Form {...formItemLayout}>
+        <Form.Item label="id" style={{display: 'none'}}>
+          {getFieldDecorator('id', {
+            initialValue: obj.id,
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="线路">
+          {getFieldDecorator('siteLine', {
+            initialValue: obj.siteLine,
+            rules: [{required: true}],
+          })(
+          <Select placeholder="请选择线路">
+            {props.locationTree.line && props.locationTree.line.map(item => (
+              <Select.Option key={item.value} value={item.value}>
+                {item.label}
+              </Select.Option>
+            ))}
+          </Select>)}
+        </Form.Item>
+        <Form.Item label="站点">
+          {getFieldDecorator('site', {
+            initialValue: obj.site,
+            rules: [{required: true}],
+          })(
+          <Select placeholder="请选择站点">
+            {props.locationTree.site && props.locationTree.site.map(item => (
+              <Select.Option key={item.value} value={item.value}>
+                {item.label}
+              </Select.Option>
+            ))}
+          </Select>)}
+        </Form.Item>
+        <Form.Item label="服务器IP">
+          {getFieldDecorator('ip', {
+            initialValue: obj.ip,
+            rules: [{required: true}],
+          })(<Input placeholder="请输入服务器IP"/>)}
+        </Form.Item>
+        <Form.Item label="服务器端口">
+          {getFieldDecorator('port', {
+            initialValue: obj.port,
+            rules: [{required: true}],
+          })(<Input placeholder="请输入服务器端口"/>)}
+        </Form.Item>
+        <Form.Item label="视频流推送">
+          {getFieldDecorator('cameraStreamUrl', {
+            initialValue: obj.cameraStreamUrl,
+            rules: [{required: true}],
+          })(<Input placeholder="请输入视频流推送"/>)}
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default Form.create()(ConfigModal);
